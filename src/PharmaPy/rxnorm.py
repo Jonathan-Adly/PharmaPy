381b2eaf73eb0b2b0c0d75398b660860d2aec76b
Jeff Tagle <jeffreyrftagle@gmail.com>
2023-07-20 19:15:50 -0400

    Added function in rxnorm.py that takes a generic drug name as input, iterates through the nested dictionaries and lists, adds all possible name/dose/dosage form combinations, and prints to an indented list.

--- src/PharmaPy/rxnorm.py
+++ src/PharmaPy/rxnorm.py
@@ -1,5 +1,27 @@
 import requests
+import json
 
-url = "https://rxnav.nlm.nih.gov/REST/drugs.json?name=azithromycin"
-response = requests.get(url)
-print(response.json())
+
+def get_all_formulations(drug_name):
+    url = "https://rxnav.nlm.nih.gov/REST/drugs.json?name=" + drug_name
+    response = requests.get(url)
+    var = json.loads(response.text)
+    drug_names = []
+
+    for item in var["drugGroup"]["conceptGroup"]:
+        if "conceptProperties" in item:
+            i = 0
+            while i < len(item["conceptProperties"]):
+                drug_names.append(item["conceptProperties"][i]["name"])
+                i = i + 1
+
+    print(json.dumps(drug_names, indent=4))
+
+
+get_all_formulations("acetaminophen")
+
+
+# Use this code to test getting REST API for certain drugs.
+# url = "https://rxnav.nlm.nih.gov/REST/drugs.json?name=lisinopril"
+# var = json.loads(requests.get(url).text)
+# print(json.dumps(var, indent=4))
