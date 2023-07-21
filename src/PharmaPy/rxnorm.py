cdb562369a09b819f8f167db784155bd42f74825
Jonathan Adly <gadly0123@gmail.com>
2023-07-21 12:30:28 -0400

    fixed

--- src/PharmaPy/rxnorm.py
+++ src/PharmaPy/rxnorm.py
@@ -1,27 +1,19 @@
 import requests
-import json
-
 
 def get_all_formulations(drug_name):
-    url = "https://rxnav.nlm.nih.gov/REST/drugs.json?name=" + drug_name
-    response = requests.get(url)
-    var = json.loads(response.text)
+    url = f"https://rxnav.nlm.nih.gov/REST/drugs.json?name={drug_name}"
+    response = requests.get(url).json()
+ 
     drug_names = []
 
-    for item in var["drugGroup"]["conceptGroup"]:
+    for item in response["drugGroup"]["conceptGroup"]:
         if "conceptProperties" in item:
             i = 0
             while i < len(item["conceptProperties"]):
                 drug_names.append(item["conceptProperties"][i]["name"])
                 i = i + 1
 
-    print(json.dumps(drug_names, indent=4))
+    return drug_names
 
 
-get_all_formulations("acetaminophen")
 
-
-# Use this code to test getting REST API for certain drugs.
-# url = "https://rxnav.nlm.nih.gov/REST/drugs.json?name=lisinopril"
-# var = json.loads(requests.get(url).text)
-# print(json.dumps(var, indent=4))
