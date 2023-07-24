c7f237af6ed905f9084f38ad9577077bc2b2bd2a
Jonathan Adly <gadly0123@gmail.com>
2023-07-23 21:53:31 -0400

    nadac average

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ We have the following modules:
 
 ## A Named-Entity Recognition (NER) machine learning model 
 
-This extracts drug names from any text sample. Useful when you have medical notes, questions, or PDFs and you want to extract the drug name for further analysis. The model is very small wiht ~95% accuracy and can run locally or any modern CPU server. No complicated machine learning deployment needed.
+This extracts drug names from any text sample. Useful when you have medical notes, questions, or PDFs and you want to extract the drug name for further analysis. The model is very small wiht ~95% accuracy and can run locally on any modern CPU server. No complicated machine learning deployment needed.
 ### Sample Usage
 
 `from PharmaPy import ner`
@@ -23,8 +23,10 @@ This extracts drug names from any text sample. Useful when you have medical note
 ## NADAC 
 This module performs several common tasks to get drug pricing based on the NADAC dataset. Useful when you have a list of NDCs or drug name and what to get how much they cost. 
 
-### Sample USage 
+### Available functions
 
+1. get_from_ndc_list: return latest NADAC price per unit from an ndc list
+ 
 `from PharmaPy import nadac`
 
 `ndc_list = ['24385005452','46122062978']`
@@ -38,8 +40,20 @@ This module performs several common tasks to get drug pricing based on the NADAC
 [{'ndc_description': '8HR ARTHRITIS PAIN ER 650 MG', 'ndc': '46122062978', 'nadac_per_unit': '0.06987', 'pricing_unit': 'EA', 'as_of_date': '2022-12-28'}, {'ndc_description': '12HR NASAL DECONGEST ER 120 MG', 'ndc': '24385005452', 'nadac_per_unit': '0.28255', 'pricing_unit': 'EA', 'as_of_date': '2022-12-28'}]
 """
 ```
+2. get_avg_from_ndc_description: return average NADAC price across all NDCs for a drug.
 
+`from PharmaPy import nadac`
 
+`drug_name = 'atorvastatin' `
+
+`result = nadac.get_avg_from_ndc_description(drug_name)`
+
+`print(results)`
+
+```
+"""
+[{'ndc_description': 'ATORVASTATIN 80 MG TABLET', 'average_nadac_per_unit': '0.08744'}, {'ndc_description': 'ATORVASTATIN 40 MG TABLET', 'average_nadac_per_unit': '0.05733'}, {'ndc_description': 'ATORVASTATIN 20 MG TABLET', 'average_nadac_per_unit': '0.04069'}, {'ndc_description': 'ATORVASTATIN 10 MG TABLET', 'average_nadac_per_unit': '0.03081'}]
+"""
 
 
 
--- src/PharmaPy/nadac.py
+++ src/PharmaPy/nadac.py
@@ -1,6 +1,7 @@
 import requests
 import json
-
+import itertools
+from decimal import Decimal
 
 def get_from_ndc_list(ndc_list):
     """
@@ -9,7 +10,7 @@ def get_from_ndc_list(ndc_list):
     if not ndc_list:
         return []
     
-    url = "https://data.medicaid.gov/api/1/datastore/query/dfa2ab14-06c2-457a-9e36-5cb6d80f8d93/0/"
+    url = "https://data.medicaid.gov/api/1/datastore/query/4a00010a-132b-4e4d-a611-543c9521280f/0/"
     payload = {
         "keys": "true",
         "offset": "0",
@@ -19,6 +20,7 @@ def get_from_ndc_list(ndc_list):
             "nadac_per_unit",
             "pricing_unit",
             "as_of_date",
+            "effective_date"
         ],
         "conditions": [
             {
@@ -28,7 +30,7 @@ def get_from_ndc_list(ndc_list):
             }
         ],
         "sorts": [
-            {"property": "as_of_date", "order": "desc"},
+            {"property": "effective_date", "order": "desc"},
             {"property": "ndc_description", "order": "desc"},
         ],
     }
@@ -46,3 +48,77 @@ def get_from_ndc_list(ndc_list):
             seen.append(item["ndc_description"])
     
     return new_list
+
+def get_avg_from_ndc_description(drug_name, exact_match=False, starts_with=False):
+    """
+    This function takes a drug name and returns the average NADAC per unit for that drug
+    across all ndcs that match the drug name on the latest nadac files. 
+    default matching behavir is "contain", but you can also match on starts_with (by setting starts_with to True) or exact (by setting exact_match to True).
+    """
+    if exact_match and starts_with:
+        raise ValueError("Cannot have exact_match and starts_with both be True")
+    
+    if exact_match:
+        operator = "is"
+    elif starts_with:
+        operator = "starts with"
+    else:
+        operator = "contains"
+    
+    url = "https://data.medicaid.gov/api/1/datastore/query/4a00010a-132b-4e4d-a611-543c9521280f/0/"
+    payload = {
+        "keys": "true",
+        "offset": "0",
+        "properties": [
+            "ndc_description",
+            "ndc",
+            "nadac_per_unit",
+            "pricing_unit",
+            "effective_date",
+        ],
+        "conditions": [
+            {
+                "property": "ndc_description",
+                "value": drug_name,
+                "operator": operator,
+            }
+        ],
+        "sorts": [
+            {"property": "effective_date", "order": "desc"},
+            {"property": "ndc_description", "order": "desc"}
+        ],
+    }
+    headers = {
+        "Content-Type": "application/json",
+    }
+    response = requests.request("POST", url, json=payload, headers=headers).content
+    try:
+        results = json.loads(response)["results"]
+    except KeyError:
+        return []
+  
+
+    seen = []
+    new_list = []
+    latest_effective_date = results[0]["effective_date"]
+    for item in results:
+        if item["effective_date"] == latest_effective_date:
+            if not item["ndc"] in seen:
+                new_list.append(item)
+                seen.append(item["ndc"])
+   
+    new_list = [list(v) for k, v in itertools.groupby(new_list, key=lambda k: k["ndc_description"])]
+
+    result = []
+    for name_list in new_list:
+        total = 0
+        description = name_list[0]["ndc_description"]
+        for item in name_list:
+            total += Decimal(item["nadac_per_unit"])
+
+        average = total / len(name_list)
+        result.append({"ndc_description": description, "average_nadac_per_unit": str(average)})
+    return result
+    
+    
+
